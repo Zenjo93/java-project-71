f454f5d87549a8f2ec3023391dd8f681ba659b39
Vlad Berezhnoy <vl.brzn@gmail.com>
2023-03-30 08:34:59 +0300

    fix app cli

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -3,10 +3,36 @@ package hexlet.code;
 import picocli.CommandLine;
 import hexlet.code.Differ;
 
-public class App {
+import java.util.concurrent.Callable;
+
+@CommandLine.Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
+        description = "Compares two configuration files and shows a difference.")
+public class App implements Callable<Integer> {
+
+    @CommandLine.Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
+    boolean usageHelpRequested;
+
+    @CommandLine.Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
+    boolean versionInfoRequested;
+
+    @CommandLine.Option(names = { "-f", "--format" }, paramLabel = "format", defaultValue = "stylish", description = "output format [default: stylish]")
+    String format;
+
+    @CommandLine.Parameters(paramLabel = "filepath1", description = "path to first file")
+    String filepath1;
+
+    @CommandLine.Parameters(paramLabel = "filepath2", description = "path to first file")
+    String filepath2;
+
+    @Override
+    public Integer call() throws Exception { // your business logic goes here...
+            Differ.generate();
+        return 0;
+    }
 
     public static void main(String[] args) {
-        int exitCode = new CommandLine(new Differ()).execute(args);
+        int exitCode = new CommandLine(new App()).execute(args);
         System.exit(exitCode);
     }
 }
+
--- app/src/main/java/hexlet/code/Differ.java
+++ app/src/main/java/hexlet/code/Differ.java
@@ -1,43 +1,10 @@
 package hexlet.code;
 
-import picocli.CommandLine;
-import picocli.CommandLine.Command;
-import picocli.CommandLine.Option;
-import picocli.CommandLine.Parameters;
 
-import java.io.File;
-import java.math.BigInteger;
-import java.nio.file.Files;
-import java.security.MessageDigest;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.Callable;
-
-@Command(name = "gendiff", mixinStandardHelpOptions = true, version = "checksum 4.0",
-        description = "Compares two configuration files and shows a difference.")
-public class Differ implements Callable<Integer> {
-
-    @Option(names = { "-h", "--help" }, usageHelp = true, description = "Show this help message and exit.")
-    boolean usageHelpRequested;
-
-    @Option(names = {"-V", "--version"}, versionHelp = true, description = "Print version information and exit.")
-    boolean versionInfoRequested;
-
-    @Option(names = { "-f", "--format" }, paramLabel = "format", defaultValue = "stylish", description = "output format [default: stylish]")
-    String format;
-
-    @Parameters(paramLabel = "filepath1", description = "path to first file")
-    String filepath1;
-
-    @Parameters(paramLabel = "filepath2", description = "path to first file")
-    String filepath2;
-
-
-    @Override
-    public Integer call() throws Exception { // your business logic goes here...
-        return 0;
-    }
+public class Differ {
 
     public static void generate() {
+        System.out.println("Differ.generate() running");
+
     }
 }
