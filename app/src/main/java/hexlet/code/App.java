d4f279a087fd1097842f7491e4ac84dcad265aa1
Vlad Berezhnoy <vl.brzn@gmail.com>
2023-03-30 08:08:40 +0300

    add picocli start menu

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,7 +1,12 @@
 package hexlet.code;
 
+import picocli.CommandLine;
+import hexlet.code.Differ;
+
 public class App {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int exitCode = new CommandLine(new Differ()).execute(args);
+        System.exit(exitCode);
     }
 }
